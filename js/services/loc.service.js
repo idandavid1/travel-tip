4d82793029c7a4f07022aa32bd1319071324fe6a
Idan David <idand2000@gmail.com>
2022-12-21 16:50:35 +0200

    create loc

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -6,8 +6,8 @@ export const locService = {
 
 
 const locs = [
-    {id: _makeId(), name: 'Greatplace', lat: 32.047104, lng: 34.832384, weather: '', createdAt: Date.now, updatedAt: Date.now }, 
-    {id: _makeId(), name: 'Neveragain', lat: 32.047201, lng: 34.832581, weather: '', createdAt: Date.now, updatedAt: Date.now  }
+    {id: _makeId(), name: 'Greatplace', lat: 32.047104, lng: 34.832384, weather: '', createdAt: Date.now(), updatedAt: Date.now() }, 
+    {id: _makeId(), name: 'Neveragain', lat: 32.047201, lng: 34.832581, weather: '', createdAt: Date.now(), updatedAt: Date.now() }
 ]
 
 function getLocs() {
@@ -16,6 +16,18 @@ function getLocs() {
             resolve(locs)
         }, 2000)
     })
+}
+
+function createLoc(name, lat, lan) {
+    return {
+        id: _makeId(),
+        name,
+        lat,
+        lan,
+        weather: '',
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    }
 }
 
 function _makeId(length = 5) {
