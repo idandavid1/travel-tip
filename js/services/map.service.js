82ed8bebb0fd26f20b3d26e90dc0a6cb627b8e3f
Ofek Abramovitch <ofek301156@gmail.com>
2022-12-21 14:18:56 +0200

    Added API key for map

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -39,7 +39,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = '' //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyBhgU0OVYbmyW3C1-byx_BErfU5kfPVkxY' //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true
